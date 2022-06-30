548dcdb4535db7f3ec8bd30303df6420268c102b
ibrahim777764 <ibrahimnaseer896@gmail.com>
2022-06-30 13:57:13 +0500

    remove errors

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import Calculator from './Components/Calculator';
 
+// eslint-disable-next-line react/prefer-stateless-function
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
+      <div className="app">
         <Calculator />
       </div>
     );
