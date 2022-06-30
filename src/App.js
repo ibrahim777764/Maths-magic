3d2256a3a6197d41e3e44f721a819c29bfa21a9a
ibrahim777764 <ibrahimnaseer896@gmail.com>
2022-06-30 13:50:11 +0500

    updated files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Calculator from './Components/Calculator';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <Calculator />
+      </div>
+    );
+  }
+}
