5a02a6ec5c7eec5f5826167add393015299192d2
ibrahim777764 <ibrahimnaseer896@gmail.com>
2022-07-05 14:57:30 +0500

    updated app js file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
-import Calculator from './Components/Calculator';
-
-// eslint-disable-next-line react/prefer-stateless-function
-export default class App extends Component {
-  render() {
-    return (
-      <div className="app">
-        <Calculator />
+import Header from './Components/Header';
+import Home from './Components/Home';
+import CalculatorUI from './Components/CalculatorUI';
+import Quote from './Components/Quote';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+const App = () => {
+  return (
+    <Router>
+      <div className='app' id='home'>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/calculator' element={<CalculatorUI />} />
+          <Route path='/quote' element={<Quote />} />
+        </Routes>
       </div>
-    );
-  }
-}
+    </Router>
+  );
+};
+
+export default App;
